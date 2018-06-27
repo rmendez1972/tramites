f9d985001bc6f5ce72415d7da5d1c76fc9bbf435
Ismael García <igh1@hotmail.com>
2018-06-27 13:38:32 -0500

    seguimiento-tramite

--- src/pages/seguimiento-tramite/seguimiento-tramite.ts
+++ src/pages/seguimiento-tramite/seguimiento-tramite.ts
@@ -62,8 +62,7 @@ export class SeguimientoTramitePage {
     }
     else {
       const alert = this.alertCtrl.create({
-        title: 'Atento Aviso!',
-        //subTitle: 'En caso de requerir adjuntar algún archivo a tu trámite, te invitamos a hacerlo a través de tu laptop o computadora de escritorio desde nuestra pagina <a href="http://qroo.gob.mx/sedetus">http://qroo.gob.mx/sedetus</a> ',
+        title: 'Advertencia!',
         subTitle: 'No es posible generar comentario. El estatus de su trámite es: '+this.solicitud[0].status,
         buttons: ['Ok']
       });
