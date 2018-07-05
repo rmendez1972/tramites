450ad645570e01e4cfa3a664492aeb686e1ddcbe
marlongomez <marlonegomezh@gmail.com>
2018-07-05 15:16:30 -0500

    agregando ip de produccion

--- src/serviceUrl.ts
+++ src/serviceUrl.ts
@@ -25,32 +25,32 @@ constructor() {}
 
 
 
-	  			return this.URL='http://localhost:8080/tramites/controladorseguimiento?operacion=listarjson&id_solicitud=';
+	  			return this.URL='http://189.221.153.178:8080/tramites/controladorseguimiento?operacion=listarjson&id_solicitud=';
 
 	  	}
 
 	  	getUrladjuntos():String{
 
 
-	  			return this.URLadjuntos='http://localhost:8080/tramites/controladoradjunto?operacion=listarjson&id_seguimiento=';
+	  			return this.URLadjuntos='http://189.221.153.178:8080/tramites/controladoradjunto?operacion=listarjson&id_seguimiento=';
 	  	}
 
 	  	getUrladjuntosdescarga():String{
-	  			return this.URLadjuntosdescarga='http://localhost:8080/tramites/adjuntos/';
+	  			return this.URLadjuntosdescarga='http://189.221.153.178:8080/tramites/adjuntos/';
 
 
 
 	  	}
 
 		getUrladjuntosupload():String{
-	  			return this.URLadjuntosupload='http://localhost:3001/upload/';
+	  			return this.URLadjuntosupload='http://189.221.153.178:3001/upload/';
 	  	}
 
 	  	getUrllogin():String{
 
 
 
-	  			return this.URLlogin='http://localhost:8080/tramites/controladorregistro?operacion=apilogin&username=';
+	  			return this.URLlogin='http://189.221.153.178:8080/tramites/controladorregistro?operacion=apilogin&username=';
 
 
 	  	}
@@ -60,21 +60,21 @@ constructor() {}
 
 
 
-	  			return this.URLcambiapassword='http://localhost:8080/tramites/controladorregistro?operacion=apiSolicitanteCambioPassword&id_solicitante=';
+	  			return this.URLcambiapassword='http://189.221.153.178:8080/tramites/controladorregistro?operacion=apiSolicitanteCambioPassword&id_solicitante=';
 
 
 	  	}
 
 
 		getUrlupload():String{
-	  			return this.URLupload='http://localhost:3001/upload';
+	  			return this.URLupload='http://189.221.153.178:3001/upload';
 	  	}
 
 		getUrlfilename():String{
 
 
 
-	  			return this.URLupload='http://localhost:8080/tramites/controladoradjunto?operacion=grabarfromApp&id_seguimiento=';
+	  			return this.URLupload='http://189.221.153.178:8080/tramites/controladoradjunto?operacion=grabarfromApp&id_seguimiento=';
 
 
 
@@ -85,7 +85,7 @@ constructor() {}
 
 
 
-			return this.URLpushseguimiento='http://localhost:8080/tramites/controladorseguimiento?operacion=grabarjson&observaciones=';
+			return this.URLpushseguimiento='http://189.221.153.178:8080/tramites/controladorseguimiento?operacion=grabarjson&observaciones=';
 
 
 
@@ -95,7 +95,7 @@ constructor() {}
 
 
 
-			return this.URLpushcomentario='http://localhost:8080/tramites/controladorseguimiento?operacion=grabarcomentariojson&observaciones=';
+			return this.URLpushcomentario='http://189.221.153.178:8080/tramites/controladorseguimiento?operacion=grabarcomentariojson&observaciones=';
 
 
 
@@ -105,7 +105,7 @@ constructor() {}
 		getTramite():String{
 
 
-			return this.URLTramite='http://localhost:8080/tramites/controladorregistro?operacion=listarjson&id_usuario=';
+			return this.URLTramite='http://189.221.153.178:8080/tramites/controladorregistro?operacion=listarjson&id_usuario=';
 
 
 		}
@@ -115,17 +115,17 @@ constructor() {}
 
 
 
-			return this.URLdeleteseguimiento='http://localhost:8080/tramites/controladorseguimiento?operacion=borrarjson&id_seguimiento=';
+			return this.URLdeleteseguimiento='http://189.221.153.178:8080/tramites/controladorseguimiento?operacion=borrarjson&id_seguimiento=';
 		}
 
 		getUrlupdateSeguimiento():String{
-			return this.URLupdateseguimiento='http://localhost:8080/tramites/controladorseguimiento?operacion=actualizarjson&id_seguimiento=';
+			return this.URLupdateseguimiento='http://189.221.153.178:8080/tramites/controladorseguimiento?operacion=actualizarjson&id_seguimiento=';
 
 
 		}
 
 		getStatus():String{
 
-			return this.URLstatus='http://localhost:8080/tramites/controladorregistro?operacion=statusjson';
+			return this.URLstatus='http://189.221.153.178:8080/tramites/controladorregistro?operacion=statusjson';
 		}
 }
