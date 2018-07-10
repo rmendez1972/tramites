43603a858d376da2c04a5089eba93c02efe7f901
unknown <angellaracahuich@gmail.com>
2018-07-10 11:34:44 -0500

    Corriegiendo posicion en la ubicacion

--- src/pages/ubicacion/ubicacion.ts
+++ src/pages/ubicacion/ubicacion.ts
@@ -1,21 +1,22 @@
 import { CallNumber } from '@ionic-native/call-number';
 import { Component } from '@angular/core';
-import {  NavParams, AlertController  } from 'ionic-angular';
-import { isPresent } from 'ionic-angular/util/util';
-import { Geolocation } from '@ionic-native/geolocation';
+import {  AlertController } from 'ionic-angular';
+import { Geolocation} from '@ionic-native/geolocation';
 import {
  GoogleMaps,
  GoogleMap,
  GoogleMapsEvent,
  LatLng,
  CameraPosition,
- MarkerOptions
+ MarkerOptions,
 } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'ubicacion',
   templateUrl: 'ubicacion.html'
 })
+
+
 export class Ubicar {
 
   map: GoogleMap;
@@ -23,11 +24,11 @@ export class Ubicar {
   markers: any[] = [
     {
       position:{
-        latitude: 18.49926214,
-        longitude: -88.31169829,
+        latitude: 18.499035,
+        longitude: -88.3118765,
       },
       title:'SEDETUS CHETUMAL',
-      icon: 'assets/icon/embassy.png',
+      icon: 'assets/icon/logo.png',
       animation: 'DROP',
     },
     {
@@ -36,7 +37,7 @@ export class Ubicar {
         longitude: -86.8515999,
       },
       title:'SEDETUS CANCUN 1',
-      icon: 'assets/icon/government.png',
+      icon: 'assets/icon/logo.png',
       animation: 'DROP',
     },
     {
@@ -45,61 +46,47 @@ export class Ubicar {
         longitude: -86.8223822,
       },
       title:'SEDETUS CANCUN PLAZA VIVENDI',
-      icon: 'assets/icon/government1.png',
+      icon: 'assets/icon/logo.png',
       animation: 'DROP',
     },
     {
       position:{
-        latitude: 20.4320275,
-        longitude: -87.0143395,
+        latitude: 20.5054173,
+        longitude: -86.9431962,
       },
       title:'SEDETUS COZUMEL',
-      icon: 'assets/icon/government-buildings.png',
+      icon: 'assets/icon/logo.png',
       animation: 'DROP',
     },
 
   ];
 
   constructor(
-    private geolocation: Geolocation,
     private googleMaps: GoogleMaps,
+    private geolocation: Geolocation,
     public alertCtrl: AlertController,
     private callNumber: CallNumber,
-  ) {}
+  ) {};
 
   ionViewDidLoad(){
-    this.getCurrentPosition();
-  }
-
-  getCurrentPosition(){
-    console.log('dentro de getCurrentPosition');
-    this.geolocation.getCurrentPosition()
-    .then(position => {
-      this.myPosition = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      }
-      this.loadMap();
-    })
-    .catch(error=>{
-      console.log(error);
-    })
-  }
+    this.loadMap();
+  };
 
   loadMap(){
-    // create a new map by passing HTMLElement
     let element: HTMLElement = document.getElementById('map_canvas');
 
     this.map = this.googleMaps.create(element);
     this.map.setOptions({
         controls: {
           compass: true,
+          myLocation: true,
           myLocationButton: true,
           indoorPicker: true,
           streetviewcontrol:true,
           zoom: true
         }
     });
+
     // create CameraPosition
     let position: CameraPosition<LatLng> = {
       target: new LatLng(this.myPosition.latitude, this.myPosition.longitude),
@@ -107,27 +94,21 @@ export class Ubicar {
       tilt: 30
     };
 
-    this.map.one(GoogleMapsEvent.MAP_READY).then(()=>{
-      console.log('Map is ready!');
 
-      // move the map's camera to position
+    this.map.one(GoogleMapsEvent.MAP_READY)
+    .then(() => {
+      this.getPosition();
       this.map.moveCamera(position);
-
-      let markerOptions: MarkerOptions = {
-        position: this.myPosition,
-        title: "Mi Ubicacion",
-        icon: 'assets/icon/user.png',
-        animation: 'DROP',
-      };
-
-      this.addMarker(markerOptions);
-
       this.markers.forEach(marker=>{
         this.addMarker(marker);
       });
-
+    })
+    .catch(error =>{
+      console.log(error);
     });
-  }
+
+  };
+
 
   addMarker(options){
     let markerOptions: MarkerOptions = {
@@ -136,34 +117,30 @@ export class Ubicar {
       icon: options.icon
     };
     this.map.addMarker(markerOptions);
-  }
+  };
 
-  refreshMap(){
-    let position: CameraPosition<LatLng> = {
-      target: new LatLng(this.markers[0].position.latitude, this.markers[0].position.longitude),
-      zoom: 17,
-      tilt: 30
-    };
-    this.map.moveCamera(position);
-
-    console.log("refresh");
-  }
-
-
-  //Lamadas
+  getPosition(): void{
+    this.map.getMyLocation()
+    .then(response => {
+      this.map.moveCamera({
+        target: response.latLng
+      });
+      this.map.addMarker({
+        title: 'Mi Ubicacion',
+        icon: 'assets/icon/user.png',
+        animation: 'DROP',
+        position: response.latLng
+      });
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  };
+  //Llamadas
   callJoint(telephoneNumber) {
     this.callNumber.callNumber(telephoneNumber, true)
     .then(res => console.log('Launched dialer!', res))
     .catch(err => console.log('Error launching dialer', err));
   };
-  // muestro el mensaje de alerta invitando a usar la aplicación web en caso de requerir adjuntar archivos
-  showAlert(subtitle:string='En caso de requerir adjuntar algún archivo a tu trámite, te invitamos a hacerlo a través de tu laptop o computadora de escritorio desde nuestra pagina <a href="http://qroo.gob.mx/sedetus">http://qroo.gob.mx/sedetus</a>') {
 
-    const alert = this.alertCtrl.create({
-      title: 'Atento Aviso!',
-      subTitle: subtitle,
-      buttons: ['Ok']
-    });
-    alert.present();
-  }
 }
