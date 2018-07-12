dc60296cb4e1d2b390b60b56c7b3d2bce1adf3d5
unknown <angellaracahuich@gmail.com>
2018-07-12 12:10:38 -0500

    Update

--- src/services/seguimiento.model.js
+++
@@ -1,7 +0,0 @@
-var CategoryModel = /** @class */ (function () {
-    function CategoryModel() {
-    }
-    return CategoryModel;
-}());
-export { CategoryModel };
-//# sourceMappingURL=seguimiento.model.js.map
